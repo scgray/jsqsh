acb0818f465fa75b180025e9ef5f9ad801a2d4d2
chunkm0nkey <chunkm0nkey@3159e4b0-7f9f-4729-a81f-fdbcbe2a166f>
2007-10-10 15:00:54 +0000

    Wording cleanup and removed debugging print.

--- trunk/src/java/org/sqsh/signals/SignalManager.java
+++ trunk/src/java/org/sqsh/signals/SignalManager.java
@@ -69,7 +69,6 @@ public class SignalManager
                 catch (Exception e) {
                     
                     /* IGNORED */
-                    e.printStackTrace();
                 }
             }
         }
