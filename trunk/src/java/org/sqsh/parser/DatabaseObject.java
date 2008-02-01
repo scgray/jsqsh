22dcbfcb9c301043b833fd29cf4e78a73d91c42f
chunkm0nkey <chunkm0nkey@3159e4b0-7f9f-4729-a81f-fdbcbe2a166f>
2008-02-01 13:36:57 +0000

    Refactored the query completion logic to hopefully make it easier to add more queries that can be completed.

--- trunk/src/java/org/sqsh/parser/DatabaseObject.java
+++ trunk/src/java/org/sqsh/parser/DatabaseObject.java
@@ -18,38 +18,78 @@
 package org.sqsh.parser;
 
 /**
- * Represents a table name that is referenced by a SQL statement.
+ * Representation of a database object. 
  */
 public class DatabaseObject {
 
-    private String database;
-    private String owner;
+    private String catalog;
+    private String schema;
     private String name;
-    private String alias;
+    private String column = null;
+    private String alias = null;
     
-    public DatabaseObject (String database, String owner, String name) {
+    /**
+     * Creates a database object.
+     * 
+     * @param catalog  The catalog containing the object (can be null)
+     * @param schema The schema containing the object (can be null)
+     * @param name The name of the object (can be null).
+     */
+    public DatabaseObject (String catalog, String schema, String name) {
         
-        this.database = database;
-        this.owner = owner;
+        this.catalog = catalog;
+        this.schema = schema;
         this.name = name;
+    }
+    
+    /**
+     * Creates an object from a sequence of name parts.
+     * 
+     * @param nameParts An array of parts of an object name.
+     */
+    public DatabaseObject (String []nameParts) {
+        
+        if (nameParts.length == 1) {
+            
+            name = nameParts[0];
+        }
+        else if (nameParts.length == 2) {
+            
+            schema = nameParts[0];
+            name = nameParts[1];
+        }
+        else if (nameParts.length == 3) {
+            
+            catalog = nameParts[0];
+            schema = nameParts[1];
+            name = nameParts[2];
+        }
+        else if (nameParts.length == 4) {
+            
+            catalog = nameParts[0];
+            schema = nameParts[1];
+            name = nameParts[2];
+            column = nameParts[3];
+        }
+        
     }
 
     /**
-     * @return the database containing the object that is referenced.
+     * @return the catalog containing the object that is referenced.
      *   or null if the object is unknown.
      */
-    public String getDatabase() {
+    public String getCatalog() {
         
-        return database;
+        return catalog;
     }
     
     /**
-     * @return the owner of the object that is referenced.
-     *   or null if the owner is unknown.
+     * @return the schema of the object that is referenced.
+     *   or null if the schema is unknown.
      */
-    public String getOwner() {
+    public String getSchema() {
         
-        return owner;
+        return schema;
     }
     
     /**
@@ -68,25 +108,45 @@ public class DatabaseObject {
         return alias;
     }
     
+    /**
+     * @param alias The alias for the object
+     */
     public void setAlias(String alias) {
         
         this.alias = alias;
     }
     
+    /**
+     * @return the column of the object or null if none was found.
+     */
+    public String getColumn () {
+    
+        return column;
+    }
+
+    
+    /**
+     * @param column Sets the column of the object.
+     */
+    public void setColumn (String column) {
+    
+        this.column = column;
+    }
+
     public String toString() {
         
         StringBuilder sb = new StringBuilder();
-        if (database != null) {
+        if (catalog != null) {
             
             sb.append('[')
-                .append(database)
+                .append(catalog)
                 .append(']')
                 .append('.');
         }
-        if (owner != null) {
+        if (schema != null) {
             
             sb.append('[')
-                .append(owner)
+                .append(schema)
                 .append(']')
                 .append('.');
         }
@@ -102,5 +162,45 @@ public class DatabaseObject {
         }
         
         return sb.toString();
+    }
+    
+    /**
+     * Compares two database objects for equality.
+     */
+    public boolean equals (Object o) {
+        
+        if (o instanceof DatabaseObject) {
+            
+            DatabaseObject that = (DatabaseObject) o;
+            
+            boolean matches = 
+                (that.catalog == null && this.catalog == null)
+                    || (that.catalog != null && this.catalog != null
+                            && that.catalog.equals(this.catalog));
+            
+            matches = matches && 
+                (that.schema == null && this.schema == null)
+                    || (that.schema != null && this.schema != null
+                            && that.schema.equals(this.schema));
+            
+            matches = matches && 
+                (that.name == null && this.name == null)
+                    || (that.name != null && this.name != null
+                            && that.name.equals(this.name));
+            
+            matches = matches && 
+                (that.column == null && this.column == null)
+                    || (that.column != null && this.column != null
+                            && that.column.equals(this.column));
+            
+            matches = matches && 
+                (that.alias == null && this.alias == null)
+                    || (that.alias != null && this.alias != null
+                            && that.alias.equals(this.alias));
+            
+            return matches;
+        }
+        
+        return false;
     }
 }
