9b70a880ecc30454328bd7090ed4f885a44da95d
chunkm0nkey <chunkm0nkey@3159e4b0-7f9f-4729-a81f-fdbcbe2a166f>
2008-02-04 20:37:49 +0000

    Added logic to check to see if the session is interactive or not.

--- src/java/org/sqsh/Session.java
+++ src/java/org/sqsh/Session.java
@@ -23,7 +23,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.net.URL;
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
