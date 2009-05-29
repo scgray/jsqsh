fe7335e10534427667e031a6876cf68ec142d0cd
chunkm0nkey <chunkm0nkey@3159e4b0-7f9f-4729-a81f-fdbcbe2a166f>
2009-05-29 15:27:15 +0000

    Added dist-sol-sparc build target.

--- trunk/dist/unix/jsqsh.sh
+++ trunk/dist/unix/jsqsh.sh
@@ -7,10 +7,42 @@
 #
 
 #
+# This determines the location of this shell script.
+#
+prog=$0
+if [ ! -f "$prog" ]; then
+   prog=`command -v "$0"`
+fi
+PROG_DIR=`dirname "$prog"`
+PROG_DIR=`cd "$PROG_DIR" && pwd`
+
+#
+# These are the places that we will look for our JNI files that
+# we need to run (well, don't need, but are useful).
+#
+RL_JNI="/usr/lib/jni /usr/lib /usr/lib64 /usr/lib/libreadline-java"
+
+#
+# If there is a ../lib relative to this script directory, then
+# check there.
+#
+if [ -d "${PROG_DIR}/../lib" ]; then
+   D=`cd "${PROG_DIR}/../lib"; pwd`
+   RL_JNI="$D $RL_JNI"
+fi
+
+#
+# Ditto for ../lib/jni
+#
+if [ -d "${PROG_DIR}/../lib/jni" ]; then
+   D=`cd "${PROG_DIR}/../lib/jni"; pwd`
+   RL_JNI="$D $RL_JNI"
+fi
+
+#
 # The debian distributions like to put the JNI DLL's
 # under /usr/lib/jni
 #
-RL_JNI="/usr/lib/jni /usr/lib /usr/lib64 /usr/lib/libreadline-java"
 for dir in $RL_JNI; do
     READLINE_FILES=`ls $dir/libJavaReadline*.so 2>/dev/null`
     if [ "$READLINE_FILES" != "" ]; then
@@ -28,12 +60,18 @@ for dir in $RL_JNI; do
 done
 export LD_LIBRARY_PATH
 
+
 #
 # Encorporate all of the jars that typically come with 
 # jsqsh in its standard installation directory.
 #
-for jar in /usr/share/jsqsh/*.jar; do
-    CLASSPATH="$CLASSPATH:$jar"
+for jardir in /usr/share/jsqsh "${PROG_DIR}/../share/jsqsh"; do
+   if [ -d "${jardir}" ]; then
+      for jar in "${jardir}"/*.jar; do
+          CLASSPATH="$CLASSPATH:$jar"
+      done
+      break
+   fi
 done
 
 #
@@ -42,10 +80,13 @@ done
 #
 RL_IMPLS="/usr/lib/java /usr/share/java /usr/lib/libreadline-java"
 for dir in $RL_IMPLS; do
-    if [ -e "$dir/libreadline-java.jar" ]; then
+    if [ -f "$dir/libreadline-java.jar" ]; then
         CLASSPATH="$CLASSPATH:$dir/libreadline-java.jar"
     fi
 done
 export CLASSPATH
+
+# echo "LD_LIBRARY_PATH = $LD_LIBRARY_PATH"
+# echo "CLASSPATH = $CLASSPATH"
 
 java org.sqsh.JSqsh $*
