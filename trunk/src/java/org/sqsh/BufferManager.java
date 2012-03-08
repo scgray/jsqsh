aff9621d8a70fbca2cc211cd398cbafe619a20a6
chunkm0nkey <chunkm0nkey@3159e4b0-7f9f-4729-a81f-fdbcbe2a166f>
2012-03-08 01:31:10 +0000

    Checking in the following: - The fix for setting LD_LIBRARY_PATH in the jsqsh script - Undid the change I did for trying to avoid getting empty buffers in the   history, since it caused other bugs, re-did it so that empty buffers   are discarded on save. This is safer. - Fixed a NPE in the JsonFormatter.

--- trunk/src/java/org/sqsh/BufferManager.java
+++ trunk/src/java/org/sqsh/BufferManager.java
@@ -91,29 +91,6 @@ public class BufferManager
     public void addBuffer(Buffer buf) {
         
         /*
-         * This is a hack. There are cases where empty buffers are being
-         * appended into the buffer array. Technically I need to allow this
-         * because the "current" buffer needs to start out empty. But
-         * to avoid leaving turds when two empty buffers are appended,
-         * if the current tail is empty, I discard it and replace it with
-         * this one.
-         */
-        if (buffers.size() > 0) {
-            
-            Buffer prev = buffers.get(buffers.size() - 1);
-            if (prev.isEmpty(true)) {
-                
-                if (buf.getId() == -1) {
-                    
-                    buf.setId(prev.getId());
-                }
-                buffers.set(buffers.size()-1, buf);
-                current = buf;
-                return;
-            }
-        }
-        
-        /*
          * Assign an ID if the buffer doesn't already have one
          */
         if (buf.getId() == -1) {
@@ -329,10 +306,15 @@ public class BufferManager
              * history yet.
              */
             for (int i = buffers.size() - 1; i >= 0; i--) {
+
+                Buffer b = buffers.get(i);
                 
-                out.println("   <Buffer><![CDATA[");
-                out.println(buffers.get(i));
-                out.println("   ]]></Buffer>");
+                if (!b.isEmpty(true)) {
+
+                    out.println("   <Buffer><![CDATA[");
+                    out.println(buffers.get(i));
+                    out.println("   ]]></Buffer>");
+                }
             }
             
             out.println("</Buffers>");
