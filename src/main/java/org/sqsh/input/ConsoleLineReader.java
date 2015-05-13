3c44e25ddc1792955874711da0fdd1f1d0686b98
Scott Gray <sgray@us.ibm.com>
2015-05-13 15:50:37 -0400

    Fix #10: Got rid of unecessary "*" in password prompt
    Replaced the old cheesy way of hiding passwords with the
    official java.io.Console way of doing things.

--- src/main/java/org/sqsh/input/ConsoleLineReader.java
+++ src/main/java/org/sqsh/input/ConsoleLineReader.java
@@ -15,12 +15,10 @@
  */
 package org.sqsh.input;
 
+import java.io.Console;
 import java.io.EOFException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.PushbackInputStream;
 import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
 
 import org.sqsh.SqshContext;
 
@@ -259,59 +257,9 @@ public abstract class ConsoleLineReader {
     public String readPassword(String prompt)
         throws IOException {
         
-        MaskingThread maskingthread = new MaskingThread(prompt);
-        Thread thread = new Thread(maskingthread);
-        thread.start();
-        InputStream in = System.in;
-
-        char[] lineBuffer;
-        char[] buf;
-
-        buf = lineBuffer = new char[128];
-
-        int room = buf.length;
-        int offset = 0;
-        int c;
-
-        loop: while (true) {
-            switch (c = in.read()) {
-                case -1:
-                case '\n':
-                    break loop;
-
-                case '\r':
-                    int c2 = in.read();
-                    if ((c2 != '\n') && (c2 != -1)) {
-                        if (!(in instanceof PushbackInputStream)) {
-                            in = new PushbackInputStream(in);
-                        }
-                        ((PushbackInputStream) in).unread(c2);
-                    }
-                    else {
-                        break loop;
-                    }
-
-                default:
-                    if (--room < 0) {
-                        buf = new char[offset + 128];
-                        room = buf.length - offset - 1;
-                        System.arraycopy(lineBuffer, 0, buf, 0, offset);
-                        Arrays.fill(lineBuffer, ' ');
-                        lineBuffer = buf;
-                    }
-                    buf[offset++] = (char) c;
-                    break;
-            }
-        }
-        maskingthread.stopMasking();
-        if (offset == 0) {
-            
-            return null;
-        }
-        
-        String ret = new String(buf, 0, offset);
-        Arrays.fill(buf, ' ');
-        return ret;
+        Console console = System.console();
+        char []chars = console.readPassword(prompt);
+        return new String(chars);
     }
     
     /**
@@ -339,61 +287,4 @@ public abstract class ConsoleLineReader {
      */
     public abstract void writeHistory ()
         throws ConsoleException;
-    
-    /**
-     * This class attempts to erase characters echoed to the console.
-     */
-    private static class MaskingThread
-        extends Thread {
-
-        private volatile boolean stop;
-        private char echochar = '*';
-
-        /**
-         * @param prompt
-         *            The prompt displayed to the user
-         */
-        public MaskingThread(String prompt) {
-
-            System.out.print(prompt);
-        }
-
-        /**
-         * Begin masking until asked to stop.
-         */
-        public void run () {
-
-            int priority = Thread.currentThread().getPriority();
-            Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
-
-            try {
-                stop = true;
-                while (stop) {
-                    
-                    System.out.print("\010" + echochar);
-                    
-                    try {
-                        // attempt masking at this rate
-                        Thread.sleep(1);
-                    }
-                    catch (InterruptedException iex) {
-                        Thread.currentThread().interrupt();
-                        return;
-                    }
-                }
-            }
-            finally { // restore the original priority
-                
-                Thread.currentThread().setPriority(priority);
-            }
-        }
-
-        /**
-         * Instruct the thread to stop masking.
-         */
-        public void stopMasking () {
-
-            this.stop = false;
-        }
-    }
 }
